e3a0211b95b13f84ae1844dac62961e8391d96c9
Sibusiso Mbonani <113964231+SibusisoGlu@users.noreply.github.com>
2024-05-14 08:14:13 +0200

    Assets added

---
+++ Tennants/Tennants/Landing/LandingView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import MyLibrary
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
